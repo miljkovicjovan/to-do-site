5786368038e6a652dc39523b769d0cd8056dd0cb
miljkovicjovan <jovan.miljkovic.biz@gmail.com>
2021-02-12 11:02:57 +0100

    cleaning up and making header

---
+++ react-version/src/components/Header.js
@@ -0,0 +1,15 @@
+import '../styles/Header.css';
+
+function Header() {
+  return (
+    <div className="Header">
+      <header className="header">
+        <h1>
+          TO-DO
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
---
+++ react-version/src/components/Input.js
@@ -0,0 +1,15 @@
+import '../styles/Input.css';
+
+function Input() {
+  return (
+    <div className="Input">
+      <header className="input">
+        <form>
+            <input id="userInput" type="sumbit"></input>
+        </form>
+      </header>
+    </div>
+  );
+}
+
+export default Input;
